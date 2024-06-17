dbde0c104b80375b41ac86054ed6ed3b9fd8bc4d
Veronica Schembri <veronicaschembri@gmail.com>
2024-06-17 19:24:40 +0200

    update userContext e cartContext to use reducer instead of useState and useEffect

--- src/context/cart.context.jsx
+++ src/context/cart.context.jsx
@@ -1,5 +1,6 @@
-import { createContext, useState, useEffect } from "react";
+import { createContext, useReducer } from "react";
 
+import { createAction } from "../utils/reducer/reducer.utils";
 //Helper function per controllare id prodotto passato, se esiste aumenta la quantità altrimenti lo aggiunge.
 
 const addCartItem = (cartItems, productToAdd) => {
@@ -23,22 +24,6 @@ const addCartItem = (cartItems, productToAdd) => {
 
 const deleteItem = (cartItems, productToDelete) =>
   cartItems.filter(cartItem => cartItem.id !== productToDelete.id);
-
-//soluzione mia ok
-// const decreaseCartItem = (cartItems, productToRemove) => {
-//   if (productToRemove.quantity > 0) {
-//     // Mappa gli elementi del carrello per decrementare la quantità dell'elemento specificato
-//     const updatedCartItems = cartItems.map(cartItem =>
-//       cartItem.id === productToRemove.id
-//         ? { ...cartItem, quantity: cartItem.quantity - 1 }
-//         : cartItem
-//     );
-
-//     // Filtra gli elementi del carrello per rimuovere quelli con quantità zero
-//     return updatedCartItems.filter(cartItem => cartItem.quantity > 0);
-//   }
-//   return cartItems; // Se la quantità di productToRemove non è maggiore di 0, restituisce semplicemente gli elementi del carrello senza modificarli
-// };
 
 const decreaseCartItem = (cartItems, productToRemove) => {
   //FInd the cart item to remove
@@ -68,39 +53,81 @@ export const CartContext = createContext({
   cartTotal: 0,
 });
 
+const CART_ACTION_TYPES = {
+  SET_CART_ITEMS: "SET_CART_ITEMS",
+  SET_IS_CART_OPEN: "SET_IS_CART_OPEN",
+};
+
+const INITIAL_STATE = {
+  isCartOpen: false,
+  cartItems: [],
+  cartCount: 0,
+  cartTotal: 0,
+};
+
+const cartReducer = (state, action) => {
+  const { type, payload } = action;
+
+  switch (type) {
+    case CART_ACTION_TYPES.SET_CART_ITEMS:
+      return {
+        ...state,
+        ...payload,
+      };
+    case CART_ACTION_TYPES.SET_IS_CART_OPEN:
+      return {
+        ...state,
+        isCartOpen: payload,
+      };
+    default:
+      //il reduce dovrebbe ricevere solo leazioni che vogliamo, altrimenti deve darci errore
+      throw new Error(`unhandled type of ${type} in cartReducer`);
+  }
+};
 export const CartProvider = ({ children }) => {
-  const [isCartOpen, setIsCartOpen] = useState(false);
-  const [cartItems, setCartItems] = useState([]);
-  const [cartCount, setCartCount] = useState(0);
-  const [cartTotal, setCartTotal] = useState(0);
+  const [{ cartItems, isCartOpen, cartCount, cartTotal }, dispatch] =
+    useReducer(cartReducer, INITIAL_STATE);
 
-  const addItemToCart = productToAdd => {
-    setCartItems(addCartItem(cartItems, productToAdd));
-  };
-
-  const removeItemToCart = productToRemove => {
-    setCartItems(decreaseCartItem(cartItems, productToRemove));
-  };
-
-  const deleteProduct = productToRemove => {
-    setCartItems(deleteItem(cartItems, productToRemove));
-  };
-  useEffect(() => {
-    const newCartCount = cartItems.reduce(
+  const updateCartItemsReducer = newCartItems => {
+    /* generate newCartTotal */
+    const newCartCount = newCartItems.reduce(
       (total, cartItem) => total + cartItem.quantity,
       0
     );
-
-    setCartCount(newCartCount);
-  }, [cartItems]);
-
-  useEffect(() => {
-    const newCartTotal = cartItems.reduce(
-      (acc, cartItem) => acc + cartItem.quantity * cartItem.price,
+    /* generate newCartCount */
+    const newCartTotal = newCartItems.reduce(
+      (total, cartItem) => total + cartItem.quantity * cartItem.price,
       0
     );
-    setCartTotal(newCartTotal);
-  }, [cartItems]);
+
+    /* dispatch new action with payload */
+    dispatch(
+      createAction(CART_ACTION_TYPES.SET_CART_ITEMS, {
+        cartItems: newCartItems,
+        cartTotal: newCartTotal,
+        cartCount: newCartCount,
+      })
+    );
+  };
+
+  const addItemToCart = productToAdd => {
+    const newCartItems = addCartItem(cartItems, productToAdd);
+    updateCartItemsReducer(newCartItems);
+  };
+
+  const removeItemToCart = productToRemove => {
+    const newCartItems = decreaseCartItem(cartItems, productToRemove);
+    updateCartItemsReducer(newCartItems);
+  };
+
+  const deleteProduct = productToRemove => {
+    const newCartItems = deleteItem(cartItems, productToRemove);
+    updateCartItemsReducer(newCartItems);
+  };
+
+  const setIsCartOpen = bool => {
+    dispatch(createAction(CART_ACTION_TYPES.SET_IS_CART_OPEN, bool));
+  };
 
   const value = {
     isCartOpen,
--- src/context/user.context.jsx
+++ src/context/user.context.jsx
@@ -1,4 +1,6 @@
-import { createContext, useState, useEffect } from "react";
+import { createContext, useEffect, useReducer } from "react";
+
+import { createAction } from "../utils/reducer/reducer.utils";
 
 import {
   onAuthStateChangedListener,
@@ -12,9 +14,35 @@ export const UserContext = createContext({
 
 //priveder actual component, che useremo per wrappare qualsiasi componente che deve poter accedere al valore
 
+export const USER_ACTION_TYPES = {
+  SET_CURRENT_USER: "SET_CURRENT_USER",
+};
+//Creiamo una reduce function
+
+const userReducer = (state, action) => {
+  const { type, payload } = action;
+
+  switch (type) {
+    case USER_ACTION_TYPES.SET_CURRENT_USER:
+      return {
+        ...state,
+        currentUser: payload,
+      };
+    default:
+      throw new Error(`Unhandled type ${type} in userReducer`);
+  }
+};
+
+const INITIAL_STATE = {
+  currentUser: null,
+};
+
 export const UserProvider = ({ children }) => {
-  //il null lo passiamo per lo stato non per il valore del contesto
-  const [currentUser, setCurrentUser] = useState(null);
+  const [{ currentUser }, dispatch] = useReducer(userReducer, INITIAL_STATE);
+
+  const setCurrentUser = user => {
+    dispatch(createAction(USER_ACTION_TYPES.SET_CURRENT_USER, user));
+  };
   //voglio essere in grado di vedere il valore e di poter anche aggiornarne lo stato in qualsiasi componente nidificato dentro il context provider.
   const value = { currentUser, setCurrentUser };
 
